0d1cbb65fe0832957174de3544b6deaf60780c4b
Qunyi Lu <coserico@gmail.com>
2015-06-29 15:11:20 -0400

    claim

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
 import com.marlabs.trainee.HibernateUtil;
+import com.marlabs.trainee.model.Claim;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 
 /**
  * Created by Rico on 6/29/15.
  */
 public class Main {
     public static void main(String[] args) {
-        HibernateUtil.getSessionFactory();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Claim(1,"qgqmo"));
+        session.getTransaction().commit();
+        session.close();
     }
 }
--- src/main/java/com/marlabs/trainee/model/Claim.java
+++ src/main/java/com/marlabs/trainee/model/Claim.java
@@ -1,7 +1,40 @@
 package com.marlabs.trainee.model;
 
+import javax.persistence.*;
+
 /**
  * Created by Rico on 6/29/15.
  */
+@Entity
+@Table (name = "test1")
 public class Claim {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "name")
+    private String title;
+
+    public Claim(long id, String name){
+        this.id = id;
+        this.title = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
